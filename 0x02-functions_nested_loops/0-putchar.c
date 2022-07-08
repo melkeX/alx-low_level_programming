2eaff5fb328278237f1519cc0f4341e83fae8e69
melkeX <106961666+melkeX@users.noreply.github.com>
2022-07-08 21:57:08 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - print "_putchar", followed by new line
+ * main - print "_putchar" followed by new line
+ * 
  * Return: 0
  */
-
 int main(void)
 {
 	_putchar('_');
